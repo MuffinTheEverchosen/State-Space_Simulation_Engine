a6e206768e7f1731adb58adc5136bd3e2a65223b
TheCiarek <62485212+TheCiarek@users.noreply.github.com>
2026-05-29 15:57:08 +0200

    Poprawione rysowanie grafów i pełne UI

---
+++ SystemSimulation.py
@@ -0,0 +1,150 @@
+from typing import Callable
+
+import numpy as np
+from matplotlib import pyplot as plt
+from numpy.f2py.auxfuncs import throw_error
+
+from Utilities.types import Vector, Matrix
+from Utilities.utils import rk4, euler
+from classes.Signals import Signal
+from classes.StateSpaceSystem import StateSpaceSystem
+
+
+class SystemSimulation:
+    def __init__(self, step_size, simulated_system: StateSpaceSystem, rk4_method: bool = True, euler_method: bool = False, tolerance: float = 1e-5, current_time: float = 0):
+        self.current_time = current_time
+        self.step_size = step_size
+        self.system = simulated_system
+        self.history = {"time": [], "state": [], "output": []}
+        self.euler_method = euler_method
+        self.rk4_method = rk4_method
+        self.tolerance = tolerance
+
+    # Runtime
+    def _calc_step(self, state: Vector, control_input: Vector, step_size: float):
+        if self.rk4_method and not self.euler_method:
+            return rk4(self.system.get_state_change, state, control_input, step_size)
+        elif self.euler_method and not self.rk4_method:
+            return euler(self.system.get_state_change, state, control_input, step_size)
+        else:
+            raise ValueError("No method chosen")
+
+    def step(self, control_input: Signal):
+
+
+        control_input = np.array(control_input)
+        self.system.check_input_size(control_input)
+        control_input_final = control_input.copy()
+        for row in range(len(control_input)):
+            control_input_final[row][0] = self.input_validator(control_input[row][0])
+
+        output = self.system.get_output(control_input_final)
+
+        self.history["time"].append(self.current_time)
+        self.history["state"].append(self.system.current_state.copy())
+        self.history["output"].append(output.copy())
+
+        value_confirmed = False
+        current_step = 0
+        while not value_confirmed:
+            full_step = self._calc_step(self.system.current_state, control_input_final, self.step_size)
+            half_input = np.array([[row[0](self.current_time + self.step_size / 2)] for row in control_input])
+
+            half_1 = self._calc_step(self.system.current_state, control_input_final, self.step_size / 2)
+            half_2 = self._calc_step(half_1, half_input, self.step_size / 2)
+
+            error = np.max(np.abs(full_step - half_2))
+
+            if error <= self.tolerance:
+                value_confirmed = True
+                current_step = half_2
+                self.current_time += self.step_size
+
+                if error < self.tolerance / 10 and error:
+                    self.step_size = min(self.step_size * 1.2, 0.02)
+            else:
+                self.step_size /= 2
+
+        self.history["time"].append(self.current_time)
+        self.history["state"].append(self.system.current_state.copy())
+        self.history["output"].append(output.copy())
+
+        self.system.update(current_step)
+
+    def run(self, control_input: Matrix, simulation_time: float):
+        while self.current_time < simulation_time:
+            self.step(control_input)
+        self.step(control_input)
+
+    def rewind(self, number_of_steps_back: int):
+        state_to_rewind = self.history["state"][len(self.history["time"]) - number_of_steps_back - 1]
+        self.system.current_state = state_to_rewind
+        self.current_time -= number_of_steps_back * self.step_size
+
+        for steps in range(number_of_steps_back):
+            self.history["time"].pop()
+            self.history["state"].pop()
+            self.history["output"].pop()
+
+    def reset(self):
+        self.system.current_state = self.history["state"][0]
+        self.current_time = self.history["time"][0]
+
+        self.history["time"] = []
+        self.history["state"] = []
+        self.history["output"] = []
+
+    # getters
+
+    def get_state(self, time):
+        for t, s, o in zip(self.history["time"], self.history["state"]):
+            if t == time:
+                return s
+        raise ValueError("Wrong time")
+
+    def get_output(self, time):
+        for t, o in zip(self.history["time"], self.history["output"]):
+            if t == time:
+                return o
+        raise ValueError("Wrong time")
+
+    def input_validator(self, control_input):
+        if callable(control_input):
+            control_input = control_input(self.current_time)
+
+        return control_input
+
+    def __str__(self):
+        lines = []
+        i = 0
+        for t, s, o in zip(self.history["time"], self.history["state"], self.history["output"]):
+            if i == 0:
+                lines.append(f"Time: {t:.2f}s | State: {s.flatten()} | Output: {o.flatten()}")
+            i += 1
+            i = i % 50
+
+        return "\n".join(lines)
+
+
+    def plot_and_save(self, save_path):
+        if not self.history["time"]:
+            return
+        
+        plt.figure(figsize=(10, 5))
+        y = np.array(self.history["output"]).squeeze()
+        time_axis = self.history["time"]
+        
+        if y.ndim > 1:
+            for i in range(y.shape[1]):
+                plt.plot(time_axis, y[:, i], label=f'Wyjście {i}')
+        else:
+            plt.plot(time_axis, y, label='Wyjście')
+        
+        plt.title("Symulacja układu")
+        plt.xlabel("Czas [s]")
+        plt.ylabel("Amplituda")
+        plt.grid(True)
+        plt.legend()
+        
+        plt.savefig(save_path, dpi=300, bbox_inches='tight')
+        plt.close() 
--- main.py
+++ main.py
@@ -1,26 +1,190 @@
 from classes.Signals import RectangleSignal, SineSignal, TriangleSignal
 from classes.StateSpaceSystem import StateSpaceSystem
 from classes.SystemSimulation import SystemSimulation
+import numpy as np
+import customtkinter
+import tkinter
+from PIL import Image 
 
-step_size = None #fill that
-J1 = None #fill that
-J2 = None #fill that
-n1 = None #fill that
-n2 = None #fill that
-k = None #fill that
-b = None #fill that
+czas = 1
+step_size = 1
+J1 = 1
+J2 = 1
+n1 = 1 
+n2 = 1 
+k = 1
+b = 1 
 Jeq = J2 + J1 * pow(n2/n1, 2)
-tolerance = None # fill that
+tolerance = 1 
+Amp = 1
+Per = 1
 
 state_matrix = [[0, 1],[-k/Jeq, -b/Jeq]]
 input_matrix = [[0], [n2/(n1*Jeq)]]
 output_matrix = [[1, 0], [0, 1]]
-
-inital_state = [[None], [None]]
-
-
+inital_state = [[0], [1]]
 
 system = StateSpaceSystem(state_matrix, input_matrix, output_matrix, inital_state)
 simulation = SystemSimulation(step_size, system)
+
+
+Square_Signal = RectangleSignal(Amp, Per)
+Sine_Signal = SineSignal(Amp, Per)
+Triangle_Signal = TriangleSignal(Amp, Per)
+
+app = customtkinter.CTk()
+app.title("my app")
+app.geometry("1200x1000")
+main_frame = customtkinter.CTkFrame(app)
+main_frame.pack(fill="both", expand=True, padx=10, pady=10)
+
+left_frame = customtkinter.CTkFrame(main_frame, width=250)
+left_frame.pack(side="left", fill="y", padx=(0, 10))
+left_frame.pack_propagate(False)
+
+right_frame = customtkinter.CTkFrame(main_frame)
+right_frame.pack(side="right", fill="both", expand=True)
+
+
+mojlabel = customtkinter.CTkLabel(right_frame, text="Tu wyświetli się wykres")
+mojlabel.pack(fill="both", expand=True)
+current_signal = Triangle_Signal
+
+def combobox_callback(choice):
+    global current_signal
+    if choice == "Square Signal":
+        current_signal = Square_Signal
+    elif choice == "Sine Signal":
+        current_signal = Sine_Signal
+    elif choice == "Triangle Signal":
+        current_signal = Triangle_Signal
+
+def startsymulacja():
+    step_size = float(lotfi_step_size.get())
+    czas = float(lotfi_czas.get())
+    J1 = float(lotfi_J1.get())
+    J2 = float(lotfi_J2.get())
+    n1 = float(lotfi_n1.get())
+    n2 =float(lotfi_n2.get())
+    b = float(lotfi_b.get())
+    k = float(lotfi_k.get())
+    Per = float(lotfi_Per.get())
+    Per = float(lotfi_Amp.get())
+    tolerance = float(lotfi_tolerance.get())
+    Jeq = J2 + J1 * pow(n2/n1, 2)
+    state_matrix = [[0, 1], [-k/Jeq, -b/Jeq]]
+    input_matrix = [[0], [n2/(n1*Jeq)]]
+    output_matrix = [[1, 0], [0, 1]]
+    initial_state = [[0], [1]]
+    Square_Signal = RectangleSignal(Amp, Per)
+    Sine_Signal = SineSignal(Amp, Per)
+    Triangle_Signal = TriangleSignal(Amp, Per)
+    symulacja = StateSpaceSystem(state_matrix, input_matrix, output_matrix, inital_state)
+    symulacja_Env = SystemSimulation(step_size, symulacja, False, True)
+    symulacja_Env.run([[current_signal]], czas)
+    symulacja_Env.plot_and_save("wykres.png")
+    nowy_obraz = customtkinter.CTkImage(light_image=Image.open('wykres.png'), dark_image=Image.open('wykres.png'),size=(800,500))
+    mojlabel.configure(image=nowy_obraz, text="")
+
+class Lotfi(tkinter.Entry):
+    def __init__(self, master=None, **kwargs):
+        self.var = tkinter.StringVar()
+        tkinter.Entry.__init__(self, master, textvariable=self.var, **kwargs)
+        self.old_value = ''
+        self.var.trace('w', self.check)
+        self.get, self.set = self.var.get, self.var.set
+
+    def check(self, *args):
+        value = self.get()
+        if value == '':
+            self.old_value = value
+            return
+        try:
+            float(value)
+            self.old_value = value
+        except ValueError:
+            self.set(self.old_value)
+
+button = customtkinter.CTkButton(left_frame, text="Simulation Start", command=startsymulacja)
+button.pack(padx=20, pady=10, fill="x")
+
+combobox_var = customtkinter.StringVar(value="Triangle Signal")
+combobox = customtkinter.CTkComboBox(
+    master=app, 
+    values=["Square Signal", "Sine Signal", "Triangle Signal"], 
+    command=combobox_callback, 
+    variable=combobox_var
+)
+
+frame_width = right_frame.winfo_width()
+frame_height = right_frame.winfo_height()
+img_size = (min(frame_width - 40, 800), min(frame_height - 40, 500))
+
+combobox_var = customtkinter.StringVar(value="Triangle Signal")
+combobox = customtkinter.CTkComboBox(
+    master=left_frame, 
+    values=["Square Signal", "Sine Signal", "Triangle Signal"], 
+    command=combobox_callback, 
+    variable=combobox_var
+)
+combobox.pack(padx=20, pady=20, fill="x")
+
+
+
+Frame1 = customtkinter.CTkFrame(left_frame)
+Frame1.pack(padx=20, pady=10, fill="x")
+
+params_frame = customtkinter.CTkFrame(left_frame)
+params_frame.pack(padx=20, pady=10, fill="x")
+
+customtkinter.CTkLabel(Frame1, text="Amplituda: ").pack(anchor="w")
+lotfi_Amp = Lotfi(Frame1)
+lotfi_Amp.pack(fill="x", pady=(0,10))
+
+customtkinter.CTkLabel(Frame1, text="Okres: ").pack(anchor="w")
+lotfi_Per = Lotfi(Frame1)
+lotfi_Per.pack(fill="x", pady=(0,10))
+
+customtkinter.CTkLabel(params_frame, text="Czas trwania symulacji: ").pack(anchor="w")
+lotfi_czas = Lotfi(params_frame)
+lotfi_czas.pack(fill="x", pady=(0,10))
+
+customtkinter.CTkLabel(params_frame, text="Rozmiar kroku: ").pack(anchor="w")
+lotfi_step_size = Lotfi(params_frame)
+lotfi_step_size.pack(fill="x", pady=(0,10))
+
+customtkinter.CTkLabel(params_frame, text="Wartość J1: ").pack(anchor="w")
+lotfi_J1 = Lotfi(params_frame)
+lotfi_J1.pack(fill="x", pady=(0,10))
+
+
+
+customtkinter.CTkLabel(params_frame, text="Wartość J2: ").pack(anchor="w")
+lotfi_J2 = Lotfi(params_frame)
+lotfi_J2.pack(fill="x", pady=(0,10))
+
+customtkinter.CTkLabel(params_frame, text="Liczba zębów przekładni 1 (n1): ").pack(anchor="w")
+lotfi_n1 = Lotfi(params_frame)
+lotfi_n1.pack(fill="x", pady=(0,10))
+
+customtkinter.CTkLabel(params_frame, text="Liczba zębów przekładni 1 (n2): ").pack(anchor="w")
+lotfi_n2 = Lotfi(params_frame)
+lotfi_n2.pack(fill="x", pady=(0,10))
+
+customtkinter.CTkLabel(params_frame, text="Sztywność (k): ").pack(anchor="w")
+lotfi_k = Lotfi(params_frame)
+lotfi_k.pack(fill="x", pady=(0,10))
+
+customtkinter.CTkLabel(params_frame, text="Współczynnik tłumienia (b): ").pack(anchor="w")
+lotfi_b = Lotfi(params_frame)
+lotfi_b.pack(fill="x", pady=(0,10))
+
+customtkinter.CTkLabel(params_frame, text="Tolerancja: ").pack(anchor="w")
+lotfi_tolerance = Lotfi(params_frame)
+lotfi_tolerance.pack(fill="x", pady=(0,10))
+
+
+app.mainloop()
+
 
 
